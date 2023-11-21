4eb23ee60f6e1ec7b8280ec8cebc57c7cb5116ec
PaulWu <haipwu98@gmail.com>
2023-11-21 10:46:33 -0500

    update url name so its more clear

--- back-end/mastermind_backend/user/urls.py
+++ back-end/mastermind_backend/user/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("api/get_random_numbers", views.get_random_number, name="get_random_number"),
     path("api/get_all_user",views.api_get_all_user, name="api_get_all_user"),
     path(
-        "api/user/<str:username>/",
+        "api/get_user/<str:username>/",
         views.api_get_user,
         name="api_get_user",
     ),
