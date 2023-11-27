71faf7ddad1c8fb33997b8671ae70f3c186576de
PaulWu <haipwu98@gmail.com>
2023-11-27 13:39:10 -0500

    added some more css for result

--- front-end/mastermind-frontend/src/components/result.tsx
+++ front-end/mastermind-frontend/src/components/result.tsx
@@ -14,13 +14,17 @@ const Result = ({ win, setWin, openM, setOpenM, fetchNumber }: props) => {
         <div>
           <div className="fixed inset-0 flex items-center justify-center z-50">
             {win === false ? (
-              <p className="self-center px-7 py-3 mt-4 font-semibold rounded-md bg-violet-400 text-gray-900">
-                Sorry, you lose!
-              </p>
+              <div className="self-center text-center text-4xl px-7 py-3 mt-4 font-semibold rounded-md min-w-[300px] min-h-[100px] bg-violet-400 text-gray-900 mx-5">
+                Sorry!
+                <br />
+                You lose!
+              </div>
             ) : (
-              <p className="self-center px-7 py-3 mt-4 font-semibold rounded-md bg-violet-400 text-gray-900">
-                Congratulations! You win!
-              </p>
+              <div className="self-center text-center text-4xl px-7 py-3 mt-4 font-semibold rounded-md min-w-[300px] min-h-[100px] bg-violet-400 text-gray-900 mx-5">
+                Congratulations! 
+                <br />
+                You win!
+              </div>
             )}
             <button
               onClick={() => {
@@ -28,7 +32,7 @@ const Result = ({ win, setWin, openM, setOpenM, fetchNumber }: props) => {
                 fetchNumber();
                 setWin(null);
               }}
-              className="self-center px-8 py-3 mt-4 font-semibold rounded-md bg-violet-400 text-gray-900"
+              className="self-center px-8 py-3 mt-4 text-2xl font-semibold rounded-md  min-h-[100px] min-w-[300px] bg-violet-400 text-gray-900"
             >
               Start a New game
             </button>
